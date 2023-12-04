30d9b425fa1e0b417084a791f20eb396eb0aeac6
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2023-12-04 22:55:33 +0200

    start do a history block

--- js/pomadoraTimer.js
+++ js/pomadoraTimer.js
@@ -50,20 +50,22 @@ class PomadoraTimer {
         let timer = document.querySelector('#clock');
         let pomadora = document.querySelector('#pomadoro');
 
-        if (this.#startingTime === '25:00') {
+        console.log(this.#startingTime)
+
+        if (this.#startingTime === '0:10') {
             this.#row += 1;
             if (this.#row < 3) {
                 pomadora.classList.add('shortBreak');
                 pomadora.classList.remove('work');
                 pomadora.classList.remove('longBreak');
-                timer.textContent = `05:00`;
+                timer.textContent = `0:05`;
             } else if (this.#row === 3) {
                 pomadora.classList.remove('shortBreak');
                 pomadora.classList.remove('work');
                 pomadora.classList.add('longBreak');
-                timer.textContent = `15:00`;
+                timer.textContent = `00:10`;
             } else {
-                timer.textContent = '25:00'
+                timer.textContent = '0:20'
                 start.style.display = 'block';
                 reset.style.display = 'none';
                 pomadora.classList.remove('shortBreak');
@@ -71,19 +73,33 @@ class PomadoraTimer {
                 pomadora.classList.remove('longBreak');
                 this.#row = 0;
                 localStorage.setItem('maxRows', (PomadoraTimer.maxRow + 1).toString())
+
+                this.addHistory()
+
                 return;
             }
 
             PomadoraTimer.startTrue = true;
             this.start();
         } else {
-            timer.textContent = `25:00`;
+            timer.textContent = `0:10`;
             pomadora.classList.remove('shortBreak');
             pomadora.classList.add('work');
             pomadora.classList.remove('longBreak');
             PomadoraTimer.startTrue = true;
             this.start();
         }
+    }
+    addHistory(){
+        let historyBlock = document.querySelector('#historyBlock');
+        let dateClass = new Date();
+        let date = `${dateClass.getDate()}.${dateClass.getMonth() + 1}.${dateClass.getFullYear()}`;
+        let p = document.createElement("p")
+        let span = document.createElement("span")
+        span.textContent = date;
+        p.textContent = `${PomadoraTimer.maxRows}`
+        p.append(span)
+        historyBlock.append(p)
     }
 }
 
