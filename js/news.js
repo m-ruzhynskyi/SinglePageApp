2dc84488404762d5fea3956d9402e4e8c43886a0
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2023-12-20 13:54:08 +0200

    again fixed news x2

--- js/news.js
+++ js/news.js
@@ -1,5 +1,5 @@
 class NewsGenerate {
-    #url = 'httpss://newsapi.org/v2/everything?q=ukraine&sortBy=popularity&apiKey=3c0a771f45234c76a6f0a996adb1d43a';
+    #url = 'https://newsapi.org/v2/everything?q=ukraine&sortBy=popularity&apiKey=3c0a771f45234c76a6f0a996adb1d43a';
     #articles = [];
 
     // create an object with objects with news
@@ -26,28 +26,28 @@ class NewsGenerate {
     init = () => {
         this.#articles.forEach(article => {
             let newNewsDiv = document.createElement('div');
-            newNewsDiv.classList.add('newNewsBlock')
+            newNewsDiv.classList.add('newNewsBlock');
 
-            let textDiv = document.createElement('div')
-            textDiv.classList.add('textDiv')
+            let textDiv = document.createElement('div');
+            textDiv.classList.add('textDiv');
 
             let header = document.createElement('a');
             (article.title.indexOf('|') !== -1) ? header.textContent = article.title.slice(article.title.indexOf('|')+2).
             substring(0, 19)+'...': header.textContent = article.title.substring(0, 19)+'...';
-            header.href = article['url']
-            header.target = '_blank'
-            header.classList.add('articleHeader')
+            header.href = article['url'];
+            header.target = '_blank';
+            header.classList.add('articleHeader');
 
             let img = new Image(140, 115);
             img.src = article['urlToImage'];
             let p = document.createElement('p');
-            p.textContent = article.description
+            p.textContent = article.description;
             p.classList.add('descriptions');
             (article.description.length > 100) ? p.textContent = article['description'].substring(0, 101)+'...': p.textContent = article['description'];
 
-            textDiv.append(header, p)
-            newNewsDiv.append(textDiv,img)
-            document.querySelector('.news').append(newNewsDiv)
+            textDiv.append(header, p);
+            newNewsDiv.append(textDiv,img);
+            document.querySelector('.news').append(newNewsDiv);
         })
 
     }
